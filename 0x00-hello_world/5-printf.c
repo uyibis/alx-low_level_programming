4b873e09e007051ce7f28dba4e705a8dd63795bd
uyibis <uyibis@gmail.com>
2022-10-13 17:44:40 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
- * main - Entry point
- *
- * Return: 0 ->Success
- */
-
-int main(void) 
+ main - Entry point
+ Return: 0 ->Success
+*/
+int main(void)
 {
   printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-  return (0); 
+  return (0);
 }
