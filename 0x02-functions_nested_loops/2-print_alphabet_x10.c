85f50d2c2c1396c83b6c22c1d62c959ea2fb094c
uyibis <uyibis@gmail.com>
2022-10-18 15:42:37 -0700

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,10 +16,10 @@ void print_alphabet_x10(void)
       c = 'a';
       while (c <= 'z')
 	{
-	  _putcar(c);
+	  _putchar(c);
 	  c++;
 	}
-      _putcar('\n');
+      _putchar('\n');
       i++;
     }
 }
