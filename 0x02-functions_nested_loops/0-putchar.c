e02a4e0368ed61315b5e624f2aad10b6b54a820f
uyibis <uyibis@gmail.com>
2022-10-18 15:22:53 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,13 @@
 
 int main(void)
 {
-  char c[] = "_putchar";
+  char c[] = "_putchar\n";
   int i;
-
   
   for (i = 0; i < 9; i++)    
     {
       _putchar(c[i]); 
     }
-
-  _putchar('\n');  
+  
   return (0);
 }
