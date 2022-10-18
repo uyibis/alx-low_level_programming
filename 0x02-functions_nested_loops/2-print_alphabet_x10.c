1e386173949c0df67016e4617dcbcb15ecc25d62
uyibis <uyibis@gmail.com>
2022-10-18 15:39:21 -0700

    d

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+  car c;
+  int i;
+
+  i = 0;
+
+  while (i < 10)
+    {
+      c = 'a';
+      while (c <= 'z')
+	{
+	  _putcar(c);
+	  c++;
+	}
+      _putcar('\n');
+      i++;
+    }
+}
