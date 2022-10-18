4736b6cf6e0aa776e3dbbf3220b658e46d9cb189
uyibis <uyibis@gmail.com>
2022-10-18 15:58:43 -0700

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _abs - calculate the absolute value of the number from zero.
+ * Return: absolute value of the input number.
+ */
+int _abs(int i)
+{
+  if (i < 0)
+    i = i * -1;
+  return (i);
+}
